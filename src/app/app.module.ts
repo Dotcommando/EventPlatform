64cac19c34889a5cd8fe78bc1596d4af747f37f2
Mike Filchushkin <m@hive.pro>
2018-03-23 14:09:30 +0300

    Добавлен компонент редактирования друга.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http'; 
 import { FriendsService } from './friends.service';
 
 import { AppComponent } from './app.component';
+import { FriendDetailComponent } from './friend-detail/friend-detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FriendDetailComponent
   ],
   imports: [
     BrowserModule,
+	FormsModule,
 	HttpModule
   ],
   providers: [FriendsService],
